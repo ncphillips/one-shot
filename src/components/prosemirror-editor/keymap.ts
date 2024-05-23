0e75202137a9bf9118cbf416af99cb0a4afba8a6
nolan <nolan@ncphi.com>
2024-05-23 06:16:45 -0300

    bold keymap + more content

--- src/components/prosemirror-editor/keymap.ts
+++ src/components/prosemirror-editor/keymap.ts
@@ -1,9 +1,13 @@
+import { toggleMark } from "prosemirror-commands";
 import { undo, redo } from "prosemirror-history";
 import { Command } from "prosemirror-state";
+import { schema } from "./schema";
 
 export function createCustomKeymap(): Record<string, Command> {
   return {
     "Mod-z": undo,
     "Mod-y": redo,
+    "Mod-b": toggleMark(schema.marks.strong),
+    "Mod-i": toggleMark(schema.marks.em),
   };
 }
